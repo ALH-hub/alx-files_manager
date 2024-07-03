0d841fe3c951775d1e198faab245a85dbf1a7508
Alhadji Oumate <alhadjioum0@gmail.com>
2024-07-03 02:03:07 +0100

    satus and stats routes

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,24 +3,24 @@ import redisClient from '../utils/redis';
 
 export const getStatus = (req, res) => {
   try {
-    const db = dbClient.isAlive;
-    const redis = redisClient.isAlive;
+    const db = dbClient.isAlive();
+    const redis = redisClient.isAlive();
 
-    res.status(200).json({ redis, db });
+    return res.status(200).json({ redis, db });
   } catch (err) {
     console.log(err);
-    res.status(500).json('server error');
+    return res.status(500).json('server error');
   }
 };
 
 export const getStats = (req, res) => {
   try {
-    const users = dbClient.nbUsers;
-    const files = dbClient.nbFiles;
+    const users = dbClient.nbUsers();
+    const files = dbClient.nbFiles();
 
-    res.status(200).json({ users, files });
+    return res.status(200).json({ users, files });
   } catch (err) {
     console.log(err);
-    res.status(500).json('server error');
+    return res.status(500).json('server error');
   }
 };
